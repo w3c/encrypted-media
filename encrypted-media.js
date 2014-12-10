b52e51008f9dd638c12707fb31c72fb35dfa8ebe
David Dorwin <ddorwin@google.com>
2014-12-10 15:28:52 -0800

    Use hyphens in the MediaKeyMessageType value names.
    This is consistent with the other enums and https://www.w3.org/Bugs/Public/show_bug.cgi?id=27317#c5.

--- encrypted-media.js
+++ encrypted-media.js
@@ -233,8 +233,8 @@
     'message': { func: eventref_helper, fragment: 'message', link_text: 'message',  },
     'encrypted': { func: eventref_helper, fragment: 'encrypted', link_text: 'encrypted',  },
 
-    'message-type-licenserequest': { func: idlref_helper, fragment: 'idl-def-MediaKeyMessageType.licenserequest', link_text: '"licenserequest"',  },
-    'message-type-licenserenewal': { func: idlref_helper, fragment: 'idl-def-MediaKeyMessageType.licenserenewal', link_text: '"licenserenewal"',  },
+    'message-type-license-request': { func: idlref_helper, fragment: 'idl-def-MediaKeyMessageType.license-request', link_text: '"license-request"',  },
+    'message-type-license-renewal': { func: idlref_helper, fragment: 'idl-def-MediaKeyMessageType.license-renewal', link_text: '"license-renewal"',  },
 
     'message-event-messagetype-attribute': { func: idlref_helper, fragment: 'widl-MediaKeyMessageEvent-messageType', link_text: 'messageType',  },
     'message-event-message-attribute': { func: idlref_helper, fragment: 'widl-MediaKeyMessageEvent-message', link_text: 'message',  },
