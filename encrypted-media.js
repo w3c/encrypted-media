424064bc22d58550380139e413677ca1dd05b0ba
David Dorwin <ddorwin@chromium.org>
2015-03-05 17:15:19 -0800

    Fix #21 - Add "status-pending" to MediaKeyStatus.

--- encrypted-media.js
+++ encrypted-media.js
@@ -206,9 +206,10 @@
     'remove-call': { func: idlref_helper, fragment: 'widl-MediaKeySession-remove-Promise-void', link_text: 'remove',  },
 
     'status-usable': { func: idlref_helper, fragment: 'idl-def-MediaKeyStatus.usable', link_text: '"usable"',  },
+    'status-expired': { func: idlref_helper, fragment: 'idl-def-MediaKeyStatus.expired', link_text: '"expired"',  },
     'status-output-downscaled': { func: idlref_helper, fragment: 'idl-def-MediaKeyStatus.output-downscaled', link_text: '"output-downscaled"',  },
     'status-output-not-allowed': { func: idlref_helper, fragment: 'idl-def-MediaKeyStatus.output-not-allowed', link_text: '"output-not-allowed"',  },
-    'status-expired': { func: idlref_helper, fragment: 'idl-def-MediaKeyStatus.expired', link_text: '"expired"',  },
+    'status-status-pending': { func: idlref_helper, fragment: 'idl-def-MediaKeyStatus.status-pending', link_text: '"status-pending"',  },
 
     'queue-message-algorithm': { func: term_helper, fragment: 'queue-message', link_text: 'queue a "message" event',  },
     'session-close-algorithm': { func: term_helper, fragment: 'session-close', link_text: 'session close',  },
